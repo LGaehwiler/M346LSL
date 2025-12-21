fb6f3977c1ce56d1866142dd75d4acb240e05d5e
Semir660 <Semir.Dehari@edu.gbssg.ch>
2025-12-21 15:42:24 +0100

    Test Mark Zuckerberg

--- ImageAnalysLambda/src/ImageAnalysLambda/obj/Release/net8.0/linux-x64/ImageAnalysLambda.AssemblyInfo.cs
+++ ImageAnalysLambda/src/ImageAnalysLambda/obj/Release/net8.0/linux-x64/ImageAnalysLambda.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImageAnalysLambda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824b9688a31ee1da0aa2936b57c7fd4ef3798673")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImageAnalysLambda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImageAnalysLambda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
